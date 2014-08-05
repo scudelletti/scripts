53c7a611c8c06b49ceaaf231b04a7069d5f358a9
Diogo Scudelletti <diogo@scudelletti.com>
2014-08-05 09:26:53 -0300

    Add Copy and Paste for OSX

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -15,6 +15,18 @@
 (require 'powerline)
 (powerline-default-theme)
 
+;; Copy and Paste Trick for OSX
+(defun copy-from-osx ()
+    (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
+
 ;; Save all backup file in this directory.
 (setq backup-directory-alist (quote ((".*" . "~/.emacs_backups/"))))
 
