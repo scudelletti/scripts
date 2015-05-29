cb6a8cd87b2ae7c5f0bcefa8c147c1f642294c0a
Diogo Scudelletti <diogo@scudelletti.com>
2015-05-29 16:13:32 -0300

    Emacs - Add idi-mode on initialisation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -169,12 +169,13 @@ This function is only necessary in window system."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(initial-frame-alist (quote ((fullscreen . maximized)))) ; Fullscreen on Startup
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(js-indent-level 2)
  '(magit-diff-use-overlays nil)
  '(ruby-align-to-stmt-keywords t)
- '(standard-indent 2)
- )
+ '(standard-indent 2))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
