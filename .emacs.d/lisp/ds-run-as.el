bd68d1f2fe9b5fdab03d831f02627423b388b3f2
Diogo Scudelletti <diogo@scudelletti.com>
2016-08-01 15:26:22 +0200

    Refactor ds-run-as function to use the same window

--- .emacs.d/lisp/ds-run-as.el
+++ .emacs.d/lisp/ds-run-as.el
@@ -5,7 +5,8 @@
 (defun ds-run-as-initialize ()
   "Initialize Binary and Path"
   (if (not (boundp 'ds-run-as-var-bin)) (setq ds-run-as-var-bin "bundle exec rspec"))
-  (if (not (boundp 'ds-run-as-var-path)) (setq ds-run-as-var-path (projectile-project-root))))
+  (if (not (boundp 'ds-run-as-var-path)) (setq ds-run-as-var-path (projectile-project-root)))
+  (if (not (boundp 'ds-run-as-var-window-name)) (setq ds-run-as-var-window-name "ds-output")))
 
 (defun ds-run-as-set-path (path)
   "Configure directory that the command will run"
@@ -17,17 +18,24 @@
   (interactive "sBin:")
   (setq ds-run-as-var-bin bin))
 
-(defun ds-run-as-shell-inner-command ()
-  "Return command that will be executed inside tmux"
-  (mapconcat 'identity `("source ~/.profile" "&&" "cd" ,ds-run-as-var-path "&&" ,ds-run-as-var-bin ,(buffer-file-name) "&&" "$SHELL")  " "))
+(defun ds-run-as-send-keys (command)
+  "Send command to tmux"
+  (shell-command (format "tmux send-keys '%s' ENTER" command)))
 
-(defun ds-run-as-shell-external-command ()
-  "Return tmux command with inner command"
-  (interactive)
-  (mapconcat 'identity `("tmux split-window '" ,(ds-run-as-shell-inner-command) "'") ""))
+(defun ds-run-as-find-or-create-window ()
+  "Select or create a window based on configured window name"
+  (shell-command
+   (format "tmux find-window %s || tmux new-window -n %s" ds-run-as-var-window-name ds-run-as-var-window-name)))
+
+(defun ds-run-as-clear-panel ()
+  "Clear panel"
+  (ds-run-as-send-keys "clear"))
 
 (defun ds-run-as ()
   "Run binary in specific folder with file as argument"
   (interactive)
   (ds-run-as-initialize)
-  (async-shell-command (ds-run-as-shell-external-command)))
+  (ds-run-as-find-or-create-window)
+  (ds-run-as-clear-panel)
+  (ds-run-as-send-keys (format "cd %s" ds-run-as-var-path))
+  (ds-run-as-send-keys (format "%s %s" ds-run-as-var-bin (buffer-file-name))))
