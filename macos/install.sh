b263c97d34a33c6c52de8199bb593c9564f98656
Diogo Scudelletti <diogo@scudelletti.com>
2016-09-24 22:34:13 +0200

    Change install script to use full path command for brew installation

--- macos/install.sh
+++ macos/install.sh
@@ -1,5 +1,5 @@
 # Brew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 #RVM
 brew install gpg
