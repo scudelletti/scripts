060cf723dad164d95f5620e8f1784b96f9d3e9ff
Diogo Scudelletti <diogo@scudelletti.com>
2016-10-21 22:06:10 +0200

    Fix PHP on web-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -401,7 +401,8 @@ This function is only necessary in window system."
 
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
