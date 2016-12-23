cbd41f061be772ced6172e05ae63fc2a95e9b97c
Diogo Scudelletti <diogo@scudelletti.com>
2016-12-23 09:54:00 +0100

    Beautify all responses on RestClient Mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -448,6 +448,14 @@ This function is only necessary in window system."
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; RestClient-Mode - Beautify JSON for all responses                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Default is nil
+(setq restclient-response-loaded-hook '(json-mode-beautify))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Yasnippet                                                        ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
