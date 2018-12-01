afa541bc8eb6b30d5a203e1a10bdbb612b4e80c8
Diogo Scudelletti <diogo@scudelletti.com>
2018-12-01 15:45:44 +0100

    Start xbindkeys after login

--- linux/install.sh
+++ linux/install.sh
@@ -6,8 +6,10 @@ sudo ln -s ~/projects/scripts/linux/keymap_custom.map /usr/local/share/kdb/keyma
 sudo ln -s ~/projects/scripts/linux/99-sysctl.conf /etc/sysctl.d/99-sysctl.conf
 
 # Setup X
+cd
 ln -s projects/scripts/linux/.xinitrc
 ln -s projects/scripts/linux/.Xmodmap
 ln -s projects/scripts/linux/.xbindkeysrc
 ln -s projects/scripts/linux/.Xresources
+ln -s projects/scripts/linux/.xprofile
 ln -s projects/scripts/linux/i3 .config/i3
