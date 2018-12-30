205ba120d6d00d1e6ab95fe3ebc9b3c111191f08
Diogo Scudelletti <diogo@scudelletti.com>
2018-12-30 13:22:48 +0100

    Fix Laptop Keyboard key swap and Ergodox

--- linux/install.sh
+++ linux/install.sh
@@ -2,6 +2,10 @@
 sudo mkdir -p /usr/local/share/kdb/keymaps
 sudo ln -s ~/projects/scripts/linux/keymap_custom.map /usr/local/share/kdb/keymaps/custom.map
 
+# Setup Keyboard
+sudo chmod 644 ~/projects/scripts/linux/xorg.conf/00-keyboard.conf
+sudo ln -s ~/projects/scripts/linux/xorg.conf/00-keyboard.conf /etc/X11/xorg.conf.d/
+
 # Setup iptables
 sudo ln -s ~/projects/study/ansible-playbooks/templates/iptables/rules.v4 /etc/iptables/iptables.rules
 sudo ln -s ~/projects/study/ansible-playbooks/templates/iptables/rules.v6 /etc/iptables/ip6tables.rules
@@ -16,7 +20,6 @@ sudo ln -s ~/projects/scripts/linux/99-sysctl.conf /etc/sysctl.d/99-sysctl.conf
 # Setup X
 cd
 ln -s projects/scripts/linux/.xinitrc
-ln -s projects/scripts/linux/.Xmodmap
 ln -s projects/scripts/linux/.xbindkeysrc
 ln -s projects/scripts/linux/.Xresources
 ln -s projects/scripts/linux/.xprofile
