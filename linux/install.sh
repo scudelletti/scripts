91d8551b7aa00fa4d9a3218aa3b9bd60a8f5db86
Diogo Scudelletti <diogo@scudelletti.com>
2019-04-28 12:33:40 +0200

    Update sudo permissions

--- linux/install.sh
+++ linux/install.sh
@@ -2,16 +2,19 @@
 sudo mkdir -p /usr/local/share/kdb/keymaps
 sudo ln -s ~/projects/scripts/linux/keymap_custom.map /usr/local/share/kdb/keymaps/custom.map
 sudo ln -s ~/projects/scripts/linux/vconsole.conf /etc/vconsole.conf
-sudo chown root:root /home/scudelletti/projects/scripts/linux/vconsole.conf
+sudo chown root:root ~/projects/scripts/linux/vconsole.conf
 
 # Setup Keyboard
 sudo chmod 644 ~/projects/scripts/linux/xorg.conf/00-keyboard.conf
 sudo chown root:root ~/projects/scripts/linux/xorg.conf/00-keyboard.conf
 sudo ln -s ~/projects/scripts/linux/xorg.conf/00-keyboard.conf /etc/X11/xorg.conf.d/
 
+# Sudoers
+su -c "cp $HOME/projects/scripts/linux/sudoers /etc/sudoers"
+
 # Setup iptables
-sudo chown root:root /home/scudelletti/projects/study/ansible-playbooks/templates/iptables/rules.v4
-sudo chown root:root /home/scudelletti/projects/study/ansible-playbooks/templates/iptables/rules.v6
+sudo chown root:root ~/projects/study/ansible-playbooks/templates/iptables/rules.v4
+sudo chown root:root ~/projects/study/ansible-playbooks/templates/iptables/rules.v6
 sudo ln -s ~/projects/study/ansible-playbooks/templates/iptables/rules.v4  /var/lib/iptables/rules-save
 sudo ln -s ~/projects/study/ansible-playbooks/templates/iptables/rules.v6  /var/lib/ip6tables/rules-save
 sudo systemctl start ip6tables-restore
