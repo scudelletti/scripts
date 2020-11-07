6fcda17ab151a20c269ad8679ad735252b6a38f4
Diogo Scudelletti <diogo@scudelletti.com>
2020-11-07 13:57:30 +0100

    Create ZSH configuration without Oh-My-ZSH

---
+++ .zshrc
@@ -0,0 +1,94 @@
+# Load theme
+source $HOME/projects/scripts/zsh_theme/scudelletti.zsh
+
+# User configuration
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# You may need to manually set your language environment
+export LANG=en_US.UTF-8
+
+# Fix Tmux Powerline Fonts
+export LC_CTYPE=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# Export OS type
+export OS_TYPE=$(uname | tr '[:upper:]' '[:lower:]')
+
+# Disable Spring
+export DISABLE_SPRING=1
+
+# Define default editor
+export EDITOR="emacs -nw"
+
+# Enable Erlang Shell History
+export ERL_AFLAGS="-kernel shell_history enabled"
+
+# Set Elixir Editor
+export ELIXIR_EDITOR="emc +__LINE__ __FILE__"
+
+# Fix Commit Signing
+export GPG_TTY=$(tty)
+
+# Aliases
+alias tmuxa="tmux a || tmux"
+alias got="git"
+alias ll="ls -lha"
+alias be="bundle exec"
+alias bi="bundle check || bundle install"
+alias ebashrc="emc ~/.bashrc"
+alias ezshrc="emc ~/.zshrc"
+alias ehosts="sudo vim /etc/hosts"
+
+if [[ $OS_TYPE = "linux" ]]; then
+  alias docker="sudo docker"
+  alias docker-compose="sudo docker-compose"
+fi
+
+# Folder's aliases
+alias projects="cd ~/projects"
+
+# Personal Functions
+function some-company() {
+  case "$1" in
+  'a')
+    cd ~/projects/some-company
+  ;;
+  'f')
+    cd ~/projects/some-company/some-folder
+  ;;
+  'help')
+    echo "some-company help Show this help info"
+    echo "some-company      Go to some-company folder"
+    echo "some-company f    Go to some-company/some-folder folder"
+  ;;
+  esac
+}
+
+function allow-i3() {
+  export I3SOCK=$(ls /run/user/1000/i3/ipc-socket.*)
+  export DISPLAY=:0
+}
+
+# ASDF Scripts
+. $HOME/.asdf/asdf.sh
+
+# Add ~/bin to Path
+PATH=$PATH:$HOME/bin
+
+# Use GPG Agent as SSH Agent
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+
+if [[ $OS_TYPE = "linux" ]]; then
+  [[ -z "$TMUX" ]] && exec tmux
+fi
+
+return 0
---
+++ zsh_theme/scudelletti.zsh
@@ -0,0 +1,127 @@
+autoload -U colors && colors
+setopt prompt_subst
+
+git_info() {
+# Git branch/tag, or name-rev if on detached head
+local GIT_LOCATION=${$((git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null)#(refs/heads/|tags/)}
+
+if [ -n "$GIT_LOCATION" ]; then
+local PREFIX="%{$fg[magenta]%}[%{$reset_color%}"
+local SUFIX="%{$fg[magenta]%}]%{$reset_color%}"
+local AHEAD="%{$fg[red]%}⇡NUM%{$reset_color%}"
+local BEHIND="%{$fg[cyan]%}⇣NUM%{$reset_color%}"
+local MERGING="%{$fg_bold[red]%}⚡︎%{$reset_color%}"
+local REBASING="%{$fg[red]%}⚡︎%{$reset_color%}"
+local UNTRACKED="%{$fg[red]%}●%{$reset_color%}"
+local MODIFIED="%{$fg[yellow]%}●%{$reset_color%}"
+local STAGED="%{$fg[green]%}●%{$reset_color%}"
+local STASH="%{$fg_bold[blue]%}¦NUM%{$reset_color%}"
+
+local -a DIVERGENCES
+local -a FLAGS
+
+local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+if [ "$NUM_AHEAD" -gt 0 ]; then
+DIVERGENCES+=( "${AHEAD//NUM/$NUM_AHEAD}" )
+fi
+
+local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
+if [ "$NUM_BEHIND" -gt 0 ]; then
+DIVERGENCES+=( "${BEHIND//NUM/$NUM_BEHIND}" )
+fi
+
+local NUM_STASH="$(git stash list --oneline 2> /dev/null | wc -l | tr -d ' ')"
+if [ "$NUM_STASH" -gt 0 ]; then
+DIVERGENCES+=( "${STASH//NUM/$NUM_STASH}" )
+fi
+
+local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+FLAGS+=( "$MERGING" )
+fi
+
+if [ -n $GIT_DIR ] && (test -r $GIT_DIR/rebase-apply || test -r $GIT_DIR/rebase-merge); then
+FLAGS+=( "$REBASING" )
+fi
+
+if ! git diff --cached --quiet 2> /dev/null; then
+FLAGS+=( "$STAGED" )
+fi
+
+if ! git diff --quiet 2> /dev/null; then
+FLAGS+=( "$MODIFIED" )
+fi
+
+if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+FLAGS+=( "$UNTRACKED" )
+fi
+
+local -a GIT_INFO
+GIT_INFO+=( $PREFIX )
+[ -n "$GIT_STATUS" ] && GIT_INFO+=( "$GIT_STATUS" )
+GIT_INFO+=( "$fg[magenta]$GIT_LOCATION%{$reset_color%} " )
+[[ ${#DIVERGENCES[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)DIVERGENCES}" )
+[[ ${#FLAGS[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)FLAGS}" )
+GIT_INFO+=( $SUFIX )
+print "${(j::)GIT_INFO}"
+fi
+}
+
+
+# We need to store the last exit status and return it on the end to avoid changing it
+__personal_hostname_ps1 () {
+local exit=$?
+
+print "%{\e[2;37;40m%}[%m]%{\e[0m%}"
+
+return $exit
+}
+
+__personal_last_exit_code_ps1() {
+local exit=$?
+
+if [ "$exit" = "0" ]; then
+print "%{$fg[green]%}[$exit]%{$reset_color%}"
+else
+print "%{$fg[red]%}[$exit]%{$reset_color%}"
+fi
+
+return $exit
+}
+
+__personal_ruby_version_ps1 () {
+local exit=$?
+
+local ruby_version=$(ruby -v | cut -f2 -d ' ' | cut -f1 -d 'p')
+
+print "%{$fg[yellow]%}[$ruby_version]%{$reset_color%}"
+
+return $exit
+}
+
+__personal_git_ps1() {
+local exit=$?
+
+print $(git_info)
+
+return $exit
+}
+
+__personal_username_ps1 () {
+local exit=$?
+
+print "%{$fg_bold[blue]%}%n%{$reset_color%}"
+
+return $exit
+}
+
+__personal_path_ps1 () {
+local exit=$?
+
+  print "%{\e[33;0;36m%}%~%{\e[0m%}"
+
+  return $exit
+}
+
+PROMPT=$'$(__personal_hostname_ps1)  $(__personal_last_exit_code_ps1)  $(__personal_ruby_version_ps1)  $(__personal_git_ps1)\
+$(__personal_username_ps1) $(__personal_path_ps1) $ '
