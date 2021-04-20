941e02dc249f3835fc8ef9de99f0a8b48ae963e6
Diogo Scudelletti <diogo@scudelletti.com>
2021-04-20 15:29:38 +0200

    Use default melpa instead of melpa state since some packages don't end up in stable

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,8 +4,7 @@
 ;; Melpa Repo                                                       ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-to-list 'package-archives
-  '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
