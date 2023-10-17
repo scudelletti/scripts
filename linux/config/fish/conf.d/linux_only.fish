6179470a7b27457cd95695f1dd887c8a167cc0da
Diogo Scudelletti <diogo@scudelletti.com>
2023-10-17 13:20:07 +0200

    Do not start tmux session automatically

--- linux/config/fish/conf.d/linux_only.fish
+++ linux/config/fish/conf.d/linux_only.fish
@@ -1,8 +1,5 @@
 set -l OS_TYPE (uname | tr '[:upper:]' '[:lower:]')
 
-if test $OS_TYPE = "linux"
-  # Fix Firefox in wayland
-  set -gx MOZ_ENABLE_WAYLAND=1
-
-  [ -z "$TMUX" ] && [ $TERM = "xterm-kitty" ] && exec tmux
-end
+#if test $OS_TYPE = "linux"
+#  [ -z "$TMUX" ] && [ $TERM = "xterm-kitty" ] && exec tmux
+#end
