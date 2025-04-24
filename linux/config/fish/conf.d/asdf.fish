e3f4ca5448ed7e4d0f1dc34d6584dc9b6aa9ec61
Diogo Scudelletti <diogo@scudelletti.com>
2025-04-24 15:59:37 +0200

    Upgrade ASDF fish config

--- linux/config/fish/conf.d/asdf.fish
+++ linux/config/fish/conf.d/asdf.fish
@@ -1,2 +1,11 @@
-source ~/.asdf/asdf.fish
-source ~/.asdf/completions/asdf.fish
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+
+set --erase _asdf_shims
