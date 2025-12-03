1ffdd4b224d2fe6339cdf8391d23da81f3f6a1d5
Diogo Scudelletti <diogo@scudelletti.com>
2025-12-03 15:17:39 +0100

    Use copyq from a different source

--- bin/ds-clipboard.sh
+++ bin/ds-clipboard.sh
@@ -4,8 +4,8 @@ while getopts ":ip" opt; do
   case $opt in
     i)
       input_text=$(</dev/stdin)
-      flatpak run com.github.hluk.copyq copy "$input_text"
-      flatpak run com.github.hluk.copyq add "$input_text"
+      copyq copy "$input_text"
+      copyq add "$input_text"
       ;;
     p)
       wl-paste --no-newline
