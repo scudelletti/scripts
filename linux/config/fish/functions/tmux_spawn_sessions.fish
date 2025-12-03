ae2a060d81755f0d9a6597b481df5a2feb5621dc
Diogo Scudelletti <diogo@scudelletti.com>
2025-12-03 15:30:08 +0100

    Spawn different tmux sessions with different requirements

--- linux/config/fish/functions/tmux_spawn_sessions.fish
+++ linux/config/fish/functions/tmux_spawn_sessions.fish
@@ -1,7 +1,12 @@
 function tmux_spawn_sessions
-  set projects ~/projects/
+  set projects_host ~/projects #~/foo ~/bar etc
+  set projects_devbox ~/projects/scripts #~/foo ~/bar etc
 
-  for project in $projects
+  for project in $projects_host
+    cd $project && tmuxp
+  end
+
+  for project in $projects_devbox
     cd $project && tmuxp devtools
   end
 
